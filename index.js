93bb1f66c9b02c1315517e3b631df4456e971b27
spolegends <dimitry.arhireeff@yandex.ru>
2025-02-13 02:31:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const startGame = () => {
         scale(1),
         anchor("center"),
     ]);
-    onClick( () => {    go("game");});
+    onClick(go("game"));
     onKeyPress("space", () => {    go("game");});
 }
 
